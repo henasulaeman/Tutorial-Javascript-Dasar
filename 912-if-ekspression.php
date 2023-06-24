1e1dbe5830e765f9c2b8cfe7d32eeab508f9775a
unknown <henasulaeman@sttbandung.ac.id>
2023-06-24 22:26:15 +0700

    menambah logic if

--- 912-if-ekspression.php
+++ 912-if-ekspression.php
@@ -7,9 +7,15 @@
 </head>
 <body>
 <script type="text/javascript">
-	const nilai = 90;
+	const nilai = 75;
 	if (nilai >80) {
-		document.writeln("Lulus");
+		document.writeln("Lulus Nilai A");
+	}else if(nilai >70){
+		document.writeln("Lulus Nilai B");
+	}else if(nilai >60){
+		document.writeln("Lulus Nilai C");
+	}else{
+		document.writeln("Try Again");
 	}
 </script>
 </body>
